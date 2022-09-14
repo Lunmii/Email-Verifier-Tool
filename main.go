3c32f1849bb38ee4353dc0c4b2edee314c0d39fa
Alo Oluwapelumi David <pelumidavid123456@gmail.com>
2022-09-14 14:16:23 +0100

    commit

--- main.go
+++ main.go
@@ -48,5 +48,19 @@ func checkDomain(domain string) {
 			break
 		}
 	}
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+	if err != nil {
+		log.Printf("ErrorL%v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v, %v, %v, %v, %v, %v\n", domain, hasMX, hasSPF, spfRecord, hasDMARC, dmarcRecord)
 
 }
